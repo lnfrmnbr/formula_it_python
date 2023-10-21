c7e4d1cc58255fa96736d3f72e76e1c4acc2b681
vika <118662766+lnfrmnbr@users.noreply.github.com>
2023-10-21 23:35:14 +0300

    Add files via upload

---
+++ Лабораторная работа 2/task2.py
@@ -0,0 +1,14 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+def num_of_money_capital(salary_, spend_, months_, increase_):
+    needed_money = 0
+    for i in range(months_):
+            needed_money += ((spend_ * ((increase_ + 1) ** i)) - salary_)
+    return int(needed_money)
+
+
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", num_of_money_capital(salary, spend, months, increase))
