369680b4cdaa411797187a3d59ba20052929703d
vika <118662766+lnfrmnbr@users.noreply.github.com>
2024-02-11 12:23:14 +0300

    +сеттеры и геттеры дочерних классов

--- Лабораторная работа 7/book.py
+++ Лабораторная работа 7/book.py
@@ -3,17 +3,31 @@ class Book:
         if not isinstance(name, str):
             raise TypeError("название книги должно быть str")
         if not isinstance(author, str):
-            raise TypeError("название книги должно быть str")
+            raise TypeError("имя автора должно быть str")
         self._name = name
         self._author = author
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
+    @name.setter
+    def name(self, new_name: str) -> None:
+        if not isinstance(new_name, str):
+            raise TypeError("название книги должно быть str")
+        else:
+            self._name = new_name
+
     @property
-    def author(self):
+    def author(self) -> str:
         return self._author
+
+    @author.setter
+    def author(self, new_author: str) -> None:
+        if not isinstance(new_author, str):
+            raise TypeError("имя автора должно быть str")
+        else:
+            self._author = new_author
 
     def __str__(self) -> str:
         return f'Книга "{self._name}"'
@@ -29,7 +43,20 @@ class PaperBook(Book):
             raise TypeError("количество страниц должно быть int")
         if pages <= 0:
             raise ValueError("количество страниц должно быть положительным числом")
-        self.pages = pages
+        self._pages = pages
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages: int) -> None:
+        if not isinstance(new_pages, int):
+            raise TypeError("количество страниц должно быть int")
+        elif new_pages <= 0:
+            raise ValueError("количество страниц должно быть положительным числом")
+        else:
+            self._pages = new_pages
 
     def __str__(self):
         return f"PaperBook: {self.name} by {self.author}, {self.pages} pages"
@@ -42,10 +69,23 @@ class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
         if not isinstance(duration, float):
-            raise TypeError("продолжительность должна быть int")
+            raise TypeError("продолжительность должна быть float")
         if duration <= 0:
             raise ValueError("продолжительность должна быть положительным числом")
-        self.duration = duration
+        self._duration = duration
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration: float) -> None:
+        if not isinstance(new_duration, float):
+            raise TypeError("продолжительность должна быть float")
+        elif new_duration <= 0:
+            raise ValueError("продолжительность должна быть положительным числом")
+        else:
+            self._duration = new_duration
 
     def __str__(self):
         return f"AudioBook: {self.name} by {self.author}, {self.duration} hours"
